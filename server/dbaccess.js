e183b7a77ffe11a760d508a502de066034fc6e8a
sergey roitman <sergeyr@sergeys-MacBook-Pro.local>
2014-09-18 23:11:40 +0300

    add new entity

--- server/dbaccess.js
+++ server/dbaccess.js
@@ -1,12 +1,41 @@
 /**
  * Created by sergeyr on 9/18/14.
  */
-var ddb = require('dynamodb').ddb({ accessKeyId: 'AKIAI5RGYGTBNUJTMYNA',
-    secretAccessKey: '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' });
+var aws = require('aws-sdk');
+aws.config.update({accessKeyId: 'AKIAI5RGYGTBNUJTMYNA', secretAccessKey: '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'});
+aws.config.update({region: 'us-west-2'});
+var dynamodb = new aws.DynamoDB();
 
-function createuser(userName){
-    ddb.listTables({}, function(err, res) {});
-    TableNames: ['users']
+
+function tableList(){
+    var params = {
+        TableName: 'users' /* required */
+    };
+    dynamodb.describeTable(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            process.exit(1);
+        } else {
+            console.log(data);
+            process.exit(0);
+        }
+    });
+
+}
+
+function createUser(username){
+    var params = {
+        username: {
+            username,
+        }
+        },
+    TableName: 'users',
+    AttributeUpdates: {
+
+    dynamodb.updateItem(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+    });
 }
 
 function deleteuser(userNmae){
@@ -20,3 +49,32 @@ function getuserdata(userName){
 function getuserbottle(){
 
 }
+
+function getUser(userName){
+
+}
+
+
+
+function listUsers(){
+
+    }
+
+function updateUser(username, userdate){
+
+    }
+
+function createSession(sessionData){
+
+    }
+
+function listSessions(){
+
+    }
+
+function getSessions(sessionIds) {
+
+}
+
+        tableList();
+
