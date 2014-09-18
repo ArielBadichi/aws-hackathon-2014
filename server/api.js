f97e5b9ddaf21200efc51544f5494167e7243036
sergey roitman <sergeyr@sergeys-MacBook-Pro.local>
2014-09-18 19:46:21 +0300

    db access

--- server/api.js
+++ server/api.js
@@ -15,3 +15,4 @@ function removeUser(uid) {
 function addBottles(uid, bottleInfo) {
 
 }
+
--- server/server.js
+++ server/server.js
@@ -21,3 +21,4 @@ server.head('/hello/:name', respond);
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
+
