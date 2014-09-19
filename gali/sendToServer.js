065c7df0ad2e7f3d8942eb90718eb1ceb1ee8b5d
Ariel Badichi <arikba@ironsrc.com>
2014-09-19 05:27:14 +0300

    post request fixes

--- gali/sendToServer.js
+++ gali/sendToServer.js
@@ -6,25 +6,28 @@ module.exports = sendData;
 function sendData(stats) {
     var sessionId = String(uuid.v4());
     var stationNumber = stats.stationNumber;
-    var numberOfBott = stats.numberOfBott;
+    var numberOfBottles = stats.numberOfBottles;
     var startTime = stats.startTime;
     var endTime = stats.endTime;
     var userName = stats.userName;
 
     var body = {
         sessionId: sessionId,
-        stationNumber: stationNumber,
-        numberOfBott: numberOfBott,
+        stationNumber: stationNumber.toString(),
+        numberOfBottles: numberOfBottles.toString(),
         startTime: startTime,
         endTime: endTime,
 	userName: userName
     };
 
-    body = JSON.stringify(body);
+    //body = JSON.stringify(body);
 
     console.log("Posting " + body);
 
-    request.post('http://192.168.11.10:8080/sessions', {body: body}, function(err, response) {
+    request.post({
+        url: 'http://192.168.11.10:8080/sessions',
+        json: body
+    }, function(err, response) {
         if (!err) {
             console.log("Post status code " + response.statusCode);
         } else {
